5d6b59918d1d6c7873143eee67c2edfd75e0fdf8
vl-blinov <blinov.va93@gmail.com>
2022-10-20 23:06:48 +0300

    feat: add eureka client to the services (client, account)

--- account/src/main/java/ru/blinov/account/AccountApplication.java
+++ account/src/main/java/ru/blinov/account/AccountApplication.java
@@ -2,7 +2,11 @@ package ru.blinov.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@RefreshScope
+@EnableEurekaClient
 @SpringBootApplication
 public class AccountApplication {
     public static void main(String[] args) {
