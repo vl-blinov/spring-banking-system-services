f8c0c706b5109cc83d968132977b42556fe9ba33
vl-blinov <blinov.va93@gmail.com>
2022-10-21 00:58:44 +0300

    feat: add AccountFeignClient to client

---
+++ client/src/main/java/ru/blinov/client/service/client/AccountFeignClient.java
@@ -0,0 +1,15 @@
+package ru.blinov.client.service.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import ru.blinov.client.web.constant.WebConstant;
+import ru.blinov.client.web.response.AccountResponse;
+
+import java.util.List;
+
+@FeignClient("account")
+public interface AccountFeignClient {
+    @GetMapping(WebConstant.VERSION_URL + "/clients/{clientId}/accounts")
+    List<AccountResponse> getAccountsByClientId(@PathVariable("clientId") Long clientId);
+}
