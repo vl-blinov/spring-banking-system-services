439d237c4ad935bdbbd9868c67e12630f372af7f
vl-blinov <blinov.va93@gmail.com>
2022-10-26 19:10:26 +0300

    feat: update AccountService

--- account/src/main/java/ru/blinov/account/service/AccountService.java
+++ account/src/main/java/ru/blinov/account/service/AccountService.java
@@ -2,14 +2,19 @@ package ru.blinov.account.service;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import ru.blinov.account.entity.Account;
+import ru.blinov.account.enums.Operation;
 import ru.blinov.account.exception.NotFoundException;
 import ru.blinov.account.mapper.AccountMapper;
 import ru.blinov.account.repository.AccountRepository;
+import ru.blinov.account.service.client.RecordFeignClient;
 import ru.blinov.account.web.request.AccountClosingRequest;
 import ru.blinov.account.web.request.AccountOpeningRequest;
+import ru.blinov.account.web.request.RecordRequest;
 import ru.blinov.account.web.response.AccountResponse;
+import ru.blinov.account.web.response.RecordResponse;
 
 import javax.transaction.Transactional;
 import java.math.BigDecimal;
@@ -25,6 +30,7 @@ public class AccountService {
 
     private AccountRepository accountRepository;
     private AccountMapper accountMapper;
+    private RecordFeignClient recordFeignClient;
 
     public AccountResponse openAccount(AccountOpeningRequest accountOpeningRequest) {
         Account account = Account.builder()
@@ -36,17 +42,32 @@ public class AccountService {
         log.info("Mapped account opening request: {}", account);
         Account openedAccount = accountRepository.save(account);
         log.info("Opened account: {}", openedAccount);
+        RecordRequest recordRequest = RecordRequest.builder()
+                .accId(openedAccount.getId())
+                .accNum(account.getAccNum())
+                .operName(Operation.OPEN.getMessage())
+                .build();
+        log.info("Record request: {}", recordRequest);
+        RecordResponse createdRecord = recordFeignClient.createRecord(recordRequest).getBody();
+        log.info("Created record response: {}", createdRecord);
         return accountMapper.AccountToAccountResponse(openedAccount);
     }
 
     public AccountResponse closeAccount(AccountClosingRequest accountClosingRequest) {
-        Long accountId = accountClosingRequest.getAccountId();
+        Long accountId = accountClosingRequest.getAccId();
         Account extractedAccount = accountRepository.findById(accountId)
                 .orElseThrow(() -> new NotFoundException("Account with ID '" + accountId + "' is not found"));
         log.info("Extracted account: {}", extractedAccount);
         extractedAccount.setCloseDate(LocalDate.now());
         Account closedAccount = accountRepository.save(extractedAccount);
         log.info("Closed account: {}", closedAccount);
+        RecordRequest recordRequest = RecordRequest.builder()
+                .accId(closedAccount.getId())
+                .accNum(closedAccount.getAccNum())
+                .operName(Operation.CLOSE.getMessage())
+                .build();
+        log.info("Record request: {}", recordRequest);
+        recordFeignClient.createRecord(recordRequest);
         return accountMapper.AccountToAccountResponse(closedAccount);
     }
 
@@ -77,12 +98,18 @@ public class AccountService {
         getAccountById(accountId);
         accountRepository.deleteById(accountId);
         log.info("Account deleted; account ID: {}", accountId);
+        recordFeignClient.deleteRecordsByAccountId(accountId);
     }
 
     public void deleteAccountsByClientId(Long clientId) {
         if(accountRepository.existsByClientId(clientId)) {
+            accountRepository.findAllByClientId(clientId)
+                    .stream()
+                    .map(Account::getId)
+                    .toList()
+                    .forEach(accountId -> recordFeignClient.deleteRecordsByAccountId(accountId));
             accountRepository.deleteAllByClientId(clientId);
-            log.info("Accounts for client ID: {} have been deleted", clientId);
+            log.info("Accounts for client ID '{}' have been deleted", clientId);
         } else {
             throw new NotFoundException("Accounts with client ID '" + clientId + "' are not found");
         }
